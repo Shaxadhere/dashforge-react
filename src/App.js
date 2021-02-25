4eb76be28644d06d204eccc90e2787a9e73f95ad
Shaxadhere <Shaxad.here@gmail.com>
2021-02-25 11:16:15 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <Home/>
+    <div>
+      <Header/>
+      <Home/>
+    </div>
   );
 }
 
